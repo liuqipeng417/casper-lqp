188d459b71e375d4ba40f7e24a26fd66faea41aa
liuqipeng <liuqipeng@youmi.net>
2015-11-20 19:13:08 +0800

    初始化

---
+++ source/js/index.js
@@ -0,0 +1,17 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $(".post-content").fitVids();
+
+    });
+
+    $('a img[border="0"]').remove();
+
+}(jQuery));
